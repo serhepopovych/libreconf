04038243abffe0fd731ed54ec69f00a977d2ba40
Serhey Popovych <serhe.popovych@gmail.com>
2019-02-26 16:37:21 +0200

    libnctl: Update submodule pointer to 583b115d5bc9
    With that submodule revision netctl-base supporting install.sh was
    introduced allowing simple package installation.
    
    Signed-off-by: Serhey Popovych <serhe.popovych@gmail.com>

--- netctl/lib/bash/libreconf.sh
+++ netctl/lib/bash/libreconf.sh
@@ -13,12 +13,12 @@ declare -a crt1_request_tools_list=(
 )
 
 # Source startup code
-. /netctl/lib/bash/crt1.sh
+. @dest@/netctl/lib/bash/crt1.sh
 
 # Source functions libraries
-. /netctl/lib/bash/libfile.sh
-. /netctl/lib/bash/liblog.sh
-. /netctl/lib/bash/libprocess.sh
+. @dest@/netctl/lib/bash/libfile.sh
+. @dest@/netctl/lib/bash/liblog.sh
+. @dest@/netctl/lib/bash/libprocess.sh
 
 ################################################################################
 # Global options/vars                                                          #
